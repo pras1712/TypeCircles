4a15eb460ee9663513e5ca4ac37d196798a1fd78
S <s>
2014-12-12 00:08:53 -0400

    {"type":"TI", "runs":"1"}

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -14,10 +14,19 @@ public class PythagoreanTheorem extends ConsoleProgram {
 		println("Enter values to compute the pythagorean theorem.");
 		double a= readDouble("Enter a: ");
 		double b= readDouble("Enter b: ");
-		double c = square(a)+ square(b);
+		double c = squareRoot(square(a)+ square(b));
 		println("c= " + c + ".");
 	}
+	
 	private double square(double x){
 		return x*x;
+	}
+	
+	private double squareRoot(double x){
+		y=5;
+		for (int i = 0; i < 100; i++) {
+			y=(y + x/y)/2;
+			}
+		return y;
 	}
 }
