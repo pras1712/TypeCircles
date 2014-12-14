488f947c7b4f05ec76610ea290e8eb488e8cab0e
S <s>
2014-12-14 13:06:19 -0400

    {"type":"TI", "runs":"1"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -25,7 +25,7 @@ public class CS106ATiles extends GraphicsProgram {
 		double cx = getWidth()/2;
 		double cy = getHeight()/2;
 		drawTileWithLabel(cx-TILE_SPACE/2.0-TILE_WIDTH/2.0, cx-TILE_SPACE/2.0-TILE_HEIGHT/2.0);
-		drawTileWithLabel(cx+TILE_SPACE/2.0+TILE_WIDTH/2.0, cx+TILE_SPACE/2.0+TILE_HEIGHT/2.0);
+		
 	}
 	
 	/** Draw tile with center (x,y) */
