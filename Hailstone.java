c498d62fbc0b4b6bd4845c8bdd27bd87ec9170dc
S <s>
2014-12-14 18:41:05 -0400

    {"type":"TI", "runs":"1"}

--- Hailstone.java
+++ Hailstone.java
@@ -18,8 +18,10 @@ public class Hailstone extends ConsoleProgram {
 			if (value == SENTINEL) break;
 			if((value % 2) == 0){
 				value= value/2;
-				println(2*value+ "is even, so I take half:" + value );
-				break;
+				println(2*value + "is even, so I take half: " + value );
+			} else{
+				value= 3*value +1;
+				println((value-1)/3 + "is odd, so I take 3n+1: " + value );
 			}
 		}
 	
