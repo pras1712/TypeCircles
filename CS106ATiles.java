c0e2d1119563ca25a0f3e0061864b1b511549a4c
S <s>
2014-12-14 12:53:49 -0400

    {"type":"TI", "runs":"0"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -14,9 +14,39 @@ public class CS106ATiles extends GraphicsProgram {
 	
 	/** Amount of space (in pixels) between tiles */
 	private static final int TILE_SPACE = 20;
+	
+	/** Width of tiles */
+	private static final int TILE_WIDTH = 20;	
+	
+	/** height of tiles */
+	private static final int TILE_HEIGHT = 20;
 
 	public void run() {
+		double cx = getWidth()/2;
+		double cy = getHeight()/2;
+		drawTileWithLabel(cx, cy);
 		
+	}
+	
+	/** Draw tile with center (x,y) */
+	
+	private void drawTile(double x, double y) {
+		GRect tile= new GRect(TILE_WIDTH, TILE_HEIGHT);
+		double a = x - TILE_WIDTH/2.0;
+		double b = y - TILE_HEIGHT/2.0;
+		add(tile, a, b);
+	}
+	
+	private void drawTileWithLabel(double x, double y){
+		drawTile(x, y);
+		drawLabel(x, y);	
+	}
+	
+	private void drawLabel(double x, double y){
+		GLabel label= new GLabel("CS106A");
+		double a = x - label.getWidth()/2.0;
+		double b = y -  label.getAscent()/2.0;
+		add(label, a, b);	
 	}
 }
 
