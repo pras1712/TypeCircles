affc70ec73db8905e086256d8eaeeb7386e90520
S <s>
2014-12-14 12:30:51 -0400

    {"type":"TI", "runs":"0"}

--- Target.java
+++ Target.java
@@ -12,6 +12,17 @@ import java.awt.*;
 
 public class Target extends GraphicsProgram {	
 	public void run() {
-		/* You fill this in. */
+		double cx = getWidth()/2;
+		double cy = getHeight()/2;
+		drawCircle(cx, cy, 72, Color.RED);
+	}
+	
+	private void drawCircle(double a, double b, double r, Color c){
+		double x = a - r;
+		double y = b - r;
+		GOval circle = new GOval(2 * r, 2 * r);
+		circle.setColor(c);
+		circle.setFilled(true);
+		add(circle, x, y);
 	}
 }
