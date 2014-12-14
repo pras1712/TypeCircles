075385f2e684ce84d783d77fe73c907bc839b976
S <s>
2014-12-14 18:44:23 -0400

    {"type":"TI", "runs":"1"}

--- Hailstone.java
+++ Hailstone.java
@@ -14,16 +14,21 @@ public class Hailstone extends ConsoleProgram {
 	
 	public void run() {
 		int value = readInt("Enter a number: ");
+		int total=0;
 		while (true) {
 			if (value == SENTINEL) break;
 			if((value % 2) == 0){
 				value= value/2;
 				println(2*value + "is even, so I take half: " + value );
+				total++;
 			} else{
 				value= 3*value +1;
 				println((value-1)/3 + "is odd, so I take 3n+1: " + value );
+				total++;
 			}
 		}
+		
+		println("The process took" +total+ "to reach 1");
 	
 	}
 }
