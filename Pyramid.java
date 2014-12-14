f3cad10914323fc746880b4c9eea9e49ed125367
S <s>
2014-12-14 01:20:55 -0400

    {"type":"TI", "runs":"1"}

--- Pyramid.java
+++ Pyramid.java
@@ -26,9 +26,9 @@ public class Pyramid extends GraphicsProgram {
 	private static final int BRICKS_IN_BASE = 14;
 	
 	public void run() {
-		double x = getWidth()/2;
-		double cy = getHeight(); 
-		buildRow(14, x, cy-BRICK_HEIGHT);
+		double cx = getWidth()/2;
+		double cy = getHeight();
+		buildRow(14, 0, cy-BRICK_HEIGHT);
 	}
 	
 	
