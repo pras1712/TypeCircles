c57d9add69b94cd05119c6142e409d8247005303
S <s>
2014-12-14 00:49:44 -0400

    {"type":"TI", "runs":"1"}

--- Pyramid.java
+++ Pyramid.java
@@ -29,7 +29,7 @@ public class Pyramid extends GraphicsProgram {
 		double cx = getWidth()/2;
 		double cy = getHeight(); 
 		for (int i = 0; i<14 ; i++) {
-			bulidRow(i,cx-i,cy-(15-i)*BRICK_HEIGHT);
+			bulidRow(i,10,cy-(15-i)*BRICK_HEIGHT);
 			}	
 	}
 	
