9d64934c1137c124ad4ff564f07f1d9dc59cf051
S <s>
2015-02-13 18:24:40 -0400

    {"type":"TI", "runs":"0"}

--- Hailstone.java
+++ Hailstone.java
@@ -21,7 +21,7 @@ public class Hailstone extends ConsoleProgram {
 			if((value % 2) == 0){
 				value/= 2;
 				println(2*value + " is even, so I take half: " + value );
-			} else{
+			} else {
 				value= 3*value +1;
 				println((value-1)/3 + " is odd, so I take 3n+1: " + value );
 			}
