4931f7028d158a75d65643a6fa14ad9e839a3391
S <s>
2015-02-13 18:29:33 -0400

    {"type":"TI", "runs":"1"}

--- FindRange.java
+++ FindRange.java
@@ -9,22 +9,22 @@
 import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
-	
+
 	private static final int SENTINEL = 0;
-	
+
 	public void run() {
 		println("This program finds the largest and smallest numbers.");
-		
+
 		/*
 		 * We set the max to be the minimum possible and the min to be the max
 		 * possible, so that after any set of numbers that is chosen, the program
 		 * knows the max and the min.
 		 */
-		
+
 		int max=Integer.MIN_VALUE;
 		int min=Integer.MAX_VALUE;
-		
-		
+
+
 		while (true) {
 			int value = readInt(" ? ");
 			if (value==SENTINEL && max==Integer.MIN_VALUE){
@@ -38,11 +38,11 @@ public class FindRange extends ConsoleProgram {
 				min=value;
 			}
 		}
-		
+
 		if (max!=Integer.MIN_VALUE && min!=Integer.MAX_VALUE){ //If any number has been put, these numbers won't both be the max and min.
-		println("smallest="+min);
-		println("largest="+max);	
-		
+			println("smallest="+min);
+			println("largest="+max);	
+
 		}
 	}
 }
