f58217c8fcbed822ff9fc0447889a6474397fa4a
S <s>
2015-02-13 18:30:35 -0400

    {"type":"TI", "runs":"0"}

--- FindRange.java
+++ FindRange.java
@@ -23,9 +23,8 @@ public class FindRange extends ConsoleProgram {
 
 		int max=Integer.MIN_VALUE;
 		int min=Integer.MAX_VALUE;
-
-		while (true) {
-			int value = readInt(" ? ");
+		int value = readInt(" ? ");
+		while (value != 0) {
 			if (value==SENTINEL && max==Integer.MIN_VALUE){
 				println("No numbers have been entered");
 			}
